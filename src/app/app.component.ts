f2dd84254573d86ff9ca57fbaf4814f9d5848592
ganeshagre <ganeshagre20@gmail.com>
2024-07-22 00:49:05 +0530

    rendering flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+interface Ichildren {
+  name: string;
+  age: number;
+}
+interface IPassenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+  checkInDate: number | null;
+  children: Ichildren[] | null;
+}
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'beginner-app';
+  passengers: IPassenger[] = [
+    {
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: null,
+    },
+    {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false,
+      checkInDate: null,
+      children: [
+        { name: 'Ted', age: 12 },
+        { name: 'Chloe', age: 7 },
+      ],
+    },
+    {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true,
+      checkInDate: 1491606000000,
+      children: null,
+    },
+    {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true,
+      checkInDate: 1488412800000,
+      children: [{ name: 'Jessica', age: 1 }],
+    },
+    {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false,
+      checkInDate: null,
+      children: null,
+    },
+  ];
 }
